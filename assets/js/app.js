e1d44ec5cbd13b3867e5c81bcc8af01dde584293
Gaby7077 <gamma@gmail.com>
2018-06-04 19:28:22 -0500

    ultimos cambios layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+
+
+  document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, options);
-    var elemstool = document.querySelectorAll('.tooltipped');
-    var instancestool = M.Tooltip.init(elemstool, options);
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
   });
 
   // Or with jQuery
 
   $(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton();
     $('.tooltipped').tooltip();
   });
-
